04d65073b7781d69f64b6134611e09e879e80902
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-11-01 10:51:11 -0400

    small fix

--- get_release_details/src/index.ts
+++ get_release_details/src/index.ts
@@ -4,33 +4,24 @@ import path from 'path'
 import fs from 'fs'
 import { buildChangelog } from './changelog'
 
-const getTags = async (): Promise<string[]> => {
+const getLastTag = async (): Promise<string> => {
   const rawTags: string = await Promise.fromCallback((cb) => exec('git rev-list --tags --max-count=30', cb))
   const tags = rawTags.trim().split('\n').join(' ')
 
   const rawRevs: string = await Promise.fromCallback((cb) => exec(`git describe --abbrev=0 --tags ${tags}`, cb))
   const revs = rawRevs.trim().split('\n')
 
-  const strTags = []
   for (let i = 0; i < revs.length; i++) {
     if (/^v\d/.test(revs[i])) {
-      strTags.push(revs[i])
+      return revs[i]
     }
   }
-
-  return strTags
 }
 
 const run = async () => {
   const { GITHUB_WORKSPACE, INPUT_PATH } = process.env
-  const tags = await getTags()
-  if (!tags.length) {
-    return
-  }
-
-  const lastReleaseTag = tags[0]
-  const lastTagVersion = lastReleaseTag.replace(/^v/, '')
-  const previousVersion = tags[1]?.replace(/^v/, '') || lastTagVersion
+  const lastReleaseTag = await getLastTag()
+  const previousVersion = lastReleaseTag.replace(/^v/, '')
 
   console.log(`::set-output name=latest_tag::${lastReleaseTag}`)
 
@@ -38,7 +29,7 @@ const run = async () => {
     const pkg = fs.readFileSync(path.resolve(INPUT_PATH || GITHUB_WORKSPACE, 'package.json'), 'utf-8')
 
     const currentVersion = JSON.parse(pkg).version
-    const isNewRelease = lastTagVersion !== currentVersion
+    const isNewRelease = previousVersion !== currentVersion
 
     console.log(`::set-output name=version::${currentVersion}`)
     console.log(`::set-output name=is_new_release::${isNewRelease}`)
