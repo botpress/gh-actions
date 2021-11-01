fbe0651bad95ce203aab165034735b8c23d7dda5
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-11-01 19:46:18 -0400

    remove dist folder

--- dist/extract_info/src/index.js
+++
@@ -1,5 +0,0 @@
-const { INPUT_BRANCH, GITHUB_REF } = process.env;
-const branchWithoutHead = (INPUT_BRANCH || GITHUB_REF).replace('refs/heads/', '');
-const branchName = branchWithoutHead.replace(/[\W_]+/g, '_');
-console.log(`::set-output name=branch::${branchWithoutHead}`);
-console.log(`::set-output name=branch_sanitized::${branchName}`);
