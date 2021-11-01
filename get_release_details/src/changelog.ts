b1e5abedba3b8086d45ff9817745834cf00a33d3
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-11-01 10:22:17 -0400

    fix(dx): remove duplicates when generating release changelog

--- get_release_details/src/changelog.ts
+++ get_release_details/src/changelog.ts
@@ -1,26 +1,41 @@
 import 'bluebird-global'
 import changelog from 'conventional-changelog'
+import { removeDuplicates } from './remove_changelog_duplicated'
+import fs from 'fs'
 
-export const buildChangelog = async () => {
-  // see options here: https://github.com/conventional-changelog/conventional-changelog/tree/master/packages
-  const changelogOts = {
-    preset: 'angular',
-    releaseCount: 1
+const fetchChangelogs = (): string | undefined => {
+  try {
+    return fs.readFileSync('CHANGELOG.md', 'utf8')
+  } catch {
+    return undefined
   }
-  const context = {}
-  const gitRawCommitsOpts = {
-    merges: null
-  }
-  const commitsParserOpts = {
-    mergePattern: /^Merge pull request #(\d+) from (.*)/gi,
-    mergeCorrespondence: ['id', 'source']
-  }
-  const changelogWriterOpts = {}
+}
 
-  let text = ''
-  const stream = changelog(changelogOts, context, gitRawCommitsOpts, commitsParserOpts, changelogWriterOpts)
-  stream.on('data', (chunk) => (text += chunk))
-  await Promise.fromCallback((cb) => stream.on('end', cb))
+export const buildChangelog = async (previousVersion: string) => {
+  const changelogFileContent = fetchChangelogs()
+  if (changelogFileContent) {
+    return removeDuplicates(changelogFileContent, previousVersion)
+  } else {
+    // see options here: https://github.com/conventional-changelog/conventional-changelog/tree/master/packages
+    const changelogOts = {
+      preset: 'angular',
+      releaseCount: 1
+    }
+    const context = {}
+    const gitRawCommitsOpts = {
+      merges: null
+    }
+    const commitsParserOpts = {
+      mergePattern: /^Merge pull request #(\d+) from (.*)/gi,
+      mergeCorrespondence: ['id', 'source']
+    }
+    const changelogWriterOpts = {}
 
-  return text.toString().replace(/\%/g, '%25').replace(/\n/g, '%0A').replace(/\r/g, '%0D')
+    let text = ''
+    const stream = changelog(changelogOts, context, gitRawCommitsOpts, commitsParserOpts, changelogWriterOpts)
+    stream.on('data', (chunk) => (text += chunk))
+    await Promise.fromCallback((cb) => stream.on('end', cb))
+
+    return text.toString().replace(/\%/g, '%25').replace(/\n/g, '%0A').replace(/\r/g, '%0D')
+  }
 }
--- get_release_details/src/index.ts
+++ get_release_details/src/index.ts
@@ -4,23 +4,33 @@ import path from 'path'
 import fs from 'fs'
 import { buildChangelog } from './changelog'
 
-const getLastTags = async () => {
+const getTags = async (): Promise<string[]> => {
   const rawTags: string = await Promise.fromCallback((cb) => exec('git rev-list --tags --max-count=30', cb))
   const tags = rawTags.trim().split('\n').join(' ')
 
   const rawRevs: string = await Promise.fromCallback((cb) => exec(`git describe --abbrev=0 --tags ${tags}`, cb))
   const revs = rawRevs.trim().split('\n')
 
+  const strTags = []
   for (let i = 0; i < revs.length; i++) {
     if (/^v\d/.test(revs[i])) {
-      return revs[i]
+      strTags.push(revs[i])
     }
   }
+
+  return strTags
 }
 
 const run = async () => {
   const { GITHUB_WORKSPACE, INPUT_PATH } = process.env
-  const lastReleaseTag = await getLastTags()
+  const tags = await getTags()
+  if (!tags.length) {
+    return
+  }
+
+  const lastReleaseTag = tags[0]
+  const lastTagVersion = lastReleaseTag.replace(/^v/, '')
+  const previousVersion = tags[1]?.replace(/^v/, '') || lastTagVersion
 
   console.log(`::set-output name=latest_tag::${lastReleaseTag}`)
 
@@ -28,17 +38,19 @@ const run = async () => {
     const pkg = fs.readFileSync(path.resolve(INPUT_PATH || GITHUB_WORKSPACE, 'package.json'), 'utf-8')
 
     const currentVersion = JSON.parse(pkg).version
-    const tagWithoutPrefix = lastReleaseTag.replace(/^v/, '')
+    const isNewRelease = lastTagVersion !== currentVersion
 
     console.log(`::set-output name=version::${currentVersion}`)
-    console.log(`::set-output name=is_new_release::${tagWithoutPrefix !== currentVersion}`)
+    console.log(`::set-output name=is_new_release::${isNewRelease}`)
+
+    // No need to generate changelogs when it's not a new release
+    const changelog = isNewRelease ? await buildChangelog(previousVersion) : ''
+
+    console.log(`::set-output name=changelog::${changelog}`)
   } catch (err) {
     console.error('Cannot process package.json')
+    throw err
   }
-
-  const changelog = await buildChangelog()
-
-  console.log(`::set-output name=changelog::${changelog}`)
 }
 
 run()
