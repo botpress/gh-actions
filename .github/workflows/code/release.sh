28d661d9502ac5eb9726ec9ff2d5822b8171d5f2
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-12-06 20:51:04 -0500

    chore(dx): fix release process

--- .github/workflows/code/release.sh
+++ .github/workflows/code/release.sh
@@ -4,10 +4,10 @@
 set -o pipefail
 set -eux
 
-git add .
-git status
+# git add .
+# git status
 git config user.name 'github-actions[bot]'
 git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
-git commit -m "Release $TAG_NAME"
+# git commit -m "Release $TAG_NAME"
 git tag -f "$TAG_NAME"
-git push origin -f "$TAG_NAME"
+git push origin "$TAG_NAME"
