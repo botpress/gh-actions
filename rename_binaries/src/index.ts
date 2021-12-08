a349e3b09f2a2964e1dae02141432d6b6f39f045
franklevasseur <francois_levasseur@hotmail.com>
2021-12-08 16:39:29 -0500

    rename parameter

--- rename_binaries/src/index.ts
+++ rename_binaries/src/index.ts
@@ -1,19 +1,21 @@
 import fs from 'fs'
 import path from 'path'
 
-const { INPUT_PATH, INPUT_BRANCH, GITHUB_REF } = process.env
+const { INPUT_PATH, INPUT_SUBNAME, GITHUB_REF } = process.env
 
 if (!fs.existsSync(INPUT_PATH)) {
   console.error("Path doesn't exist")
   process.exit(1)
 }
 
-const branchWithoutHead = (INPUT_BRANCH || GITHUB_REF).replace('refs/heads/', '')
+const branchWithoutHead = GITHUB_REF.replace('refs/heads/', '')
 const branchName = branchWithoutHead.replace(/[\W_]+/g, '_')
+
+const subName = INPUT_SUBNAME || branchName
 
 for (const fileName of fs.readdirSync(INPUT_PATH)) {
   const [name, _version, platform, arch] = fileName.split('-')
-  const newName = `${name}-${branchName}-${platform}-${arch}`
+  const newName = `${name}-${subName}-${platform}-${arch}`
 
   fs.renameSync(path.join(INPUT_PATH, fileName), path.join(INPUT_PATH, newName))
 }
