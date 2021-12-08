99319caf2387526d31e0d28f07af0d8f40aef764
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-12-08 12:27:50 -0500

    test

--- get_release_details/dist/index.js
+++ get_release_details/dist/index.js
@@ -69228,7 +69228,6 @@ class Transformer {
         this.getIssues = async (owner, repo) => {
             const token = core.getInput('token');
             for (const pull_number of this.pullRequestNumbers) {
-                console.log('pull_number', pull_number);
                 const octokit = github.getOctokit(token);
                 const pr = await octokit.rest.pulls.get({
                     owner,
