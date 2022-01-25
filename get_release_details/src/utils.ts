4b0a8a6f7c3538cb5ae265fc9e921023d64c8011
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-01-25 10:53:47 -0500

    debugging

--- get_release_details/src/utils.ts
+++ get_release_details/src/utils.ts
@@ -1,10 +1,14 @@
+import * as core from '@actions/core'
+
 export type Callback<T> = (err: any, result: T) => void
 
 export const PromiseFromCallback = async <T>(func: (callback: Callback<T>) => void) => {
+  core.info(`PromiseFromCallback called`)
+
   return new Promise<T>((resolve, reject) => {
     const callback: Callback<T> = (e, r) => {
-      console.log('err', e)
-      console.log('res', r)
+      core.info(`err ${e}`)
+      core.info(`res ${r}`)
       if (e) {
         reject(e)
       } else {
