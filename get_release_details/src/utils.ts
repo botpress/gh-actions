ee450ce39a61686c5b66c7e68ca7e2aec04d1933
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-01-25 10:47:49 -0500

    debugging

--- get_release_details/src/utils.ts
+++ get_release_details/src/utils.ts
@@ -1,11 +1,13 @@
-export type Callback<T> = (err: any, result?: T) => void
+export type Callback<T> = (err: any, result: T) => void
 
 export const PromiseFromCallback = async <T>(func: (callback: Callback<T>) => void) => {
   return new Promise<T>((resolve, reject) => {
     const callback: Callback<T> = (e, r) => {
+      console.log('err', e)
+      console.log('res', r)
       if (e) {
         reject(e)
-      } else if (r) {
+      } else {
         resolve(r)
       }
     }
