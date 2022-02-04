52e0b96c8abda8f52d8a3fe3c33c834c69164346
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-02-04 16:16:29 -0500

    fix

--- close_pull_request_issues/src/utils.ts
+++ close_pull_request_issues/src/utils.ts
@@ -1,7 +1,3 @@
-import * as core from '@actions/core'
-
-export const REPOS = JSON.parse(core.getInput('repos', { required: true })) as string[]
-
 export type Callback<T> = (err: any, result: T) => void
 
 export const PromiseFromCallback = async <T>(func: (callback: Callback<T>) => void) => {
