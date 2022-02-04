b988a44d72b6892a07b8028a71360a8dcf3395e8
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-02-04 13:53:34 -0500

    finished working on fetch release changelogs

--- fetch_release_changelogs/src/changelog.ts
+++ fetch_release_changelogs/src/changelog.ts
@@ -14,9 +14,7 @@ export const getChangelogs = async (repo: string, versions: string[]) => {
     repo
   })
 
-  // TODO: If sorted break once we hit a version that does not match
   for (const release of releases.data) {
-    core.info(`release tag: ${release.tag_name}`)
     if (release.prerelease || release.draft) {
       continue
     }
