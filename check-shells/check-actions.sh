5c620f51c9b3e8baed5f7ccb3052880bc1c35980
Kévin Frick <kevin.frick@botpress.com>
2026-02-04 12:33:32 -0500

    display errors with good lines and github errors

--- check-shells/check-actions.sh
+++ check-shells/check-actions.sh
@@ -7,36 +7,66 @@ if [ ! -d "$ROOT_DIR" ]; then
   exit 1
 fi
 
-ACTION_FILES="$(find "$ROOT_DIR" -type f -name action.yml)"
-ERRORS=0
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  GROUP="::group::"
+  END_GROUP="::endgroup::"
+  display_error() {
+    echo "::error file=$5,line=$6,endLine=$7,col=$8,endColumn=$9::[$1] SC$2: $3"
+  }
+else
+  GROUP="▶ "
+  END_GROUP=""
+  display_error() {
+    echo "[$1] SC$2: $3 ($5:$6-$7)"
+    echo -e "\t$4"
+  }
+fi
 
-for action in $ACTION_FILES; do
-  if [ "$(yq -c '.runs | has("steps")' "$action")" != "true" ]; then
+ACTION_FILES="$(find "$ROOT_DIR" -type f -name action.yml)"
+ERRORS_COUNT=0
+
+for FILE in $ACTION_FILES; do
+  if [ "$(yq -c '.runs | has("steps")' "$FILE")" != "true" ]; then
     continue
   fi
 
-  echo "::group::Checking action file: $action"
+  echo "${GROUP}Checking action file: $FILE"
 
   while IFS= read -r step; do
 
     NAME=$(echo "$step" | yq -r '.name // "Unnamed"')
-    echo "Checking step: $NAME"
+    LINE=$(echo "$step" | yq -r '.line // ""')
+  
+    echo "Checking step: $NAME ($FILE:$LINE)"
 
     # SC2148: script missing shebang
     # SC2296: github variables expansion: ${{something}}
-    if (printf '%s\n' "$step" | yq -r '.run' | shellcheck - -e SC2148 -e SC2296); then
+    ERRORS=$(printf '%s\n' "$step" | yq -r '.run' | shellcheck - -f json -e SC2148 -e SC2296)
+
+    if [ $(printf '%s\n' "$ERRORS" | yq '. | length') -eq 0 ]; then
       echo "No issues found."
     else
-      ((ERRORS++))
+      while IFS= read -r error; do
+        ERROR_LINE=$((LINE + $(echo "$error" | yq -r '.line')))
+        ERROR_ENDLINE=$((LINE + $(echo "$error" | yq -r '.endLine')))
+        ERROR_COLUMN=$(echo "$error" | yq -r '.column')
+        ERROR_ENDCOLUMN=$(echo "$error" | yq -r '.endColumn')
+        LEVEL=$(echo "$error" | yq -r '.level')
+        CODE=$(echo "$error" | yq -r '.code')
+        MESSAGE=$(echo "$error" | yq -r '.message')
+        CONTENT=$(printf '%s\n' "$step" | yq -r '.run' | sed -n "$(echo "$error" | yq -r '.line'),$(echo "$error" | yq -r '.endLine')p")
+        display_error "$LEVEL" "$CODE" "$MESSAGE" "$CONTENT" "$FILE" "$ERROR_LINE" "$ERROR_ENDLINE" "$ERROR_COLUMN" "$ERROR_ENDCOLUMN"
+        ((ERRORS_COUNT++))
+      done < <(printf '%s\n' "$ERRORS" | yq -o=json -I=0 '.[]')
     fi
 
-  done < <(yq -o=json -I=0 '.runs.steps[]? | select(has("run"))' "$action")
+  done < <(yq -o=json -I=0 '.runs.steps[]? | select(has("run")) | {"line": (.run | line), "name": .name, "run": .run}' "$FILE")
 
-  echo "::endgroup::"
+  echo "${END_GROUP}"
 done
 
-if [ "$ERRORS" -gt 0 ]; then
-  echo "There are $ERRORS action issues."
+if [ "$ERRORS_COUNT" -gt 0 ]; then
+  echo "There are $ERRORS_COUNT action issues."
   exit 1
 else
   echo "No action issues found."
